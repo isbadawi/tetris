b074fb8e7d4918c0eaa3924b4002c94581a9d15f
Ismail Badawi <ismail@badawi.io>
2016-07-12 00:11:27 -0400

    Add basic event loop.

--- tetris.cpp
+++ tetris.cpp
@@ -1,22 +1,53 @@
+#include <cstdint>
+
+#include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
-int main() {
-  sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-  sf::CircleShape shape(100.f);
-  shape.setFillColor(sf::Color::Green);
+class TetrisGame {
+private:
+  uint64_t Score;
+  uint64_t Level;
+  uint64_t Lines;
 
-  while (window.isOpen()) {
-    sf::Event event;
-    while (window.pollEvent(event)) {
-      if (event.type == sf::Event::Closed) {
-        window.close();
-      }
+public:
+  bool handleEvent(const sf::Event &Event);
+  void update(sf::Time Delta);
+  void display(sf::RenderWindow &Window);
+};
+
+bool TetrisGame::handleEvent(const sf::Event &Event) {
+  if (Event.type == sf::Event::Closed) {
+    return false;
+  }
+
+  return true;
+}
+
+void TetrisGame::update(sf::Time Delta) {
+}
+
+void TetrisGame::display(sf::RenderWindow &Window) {
+}
+
+int main() {
+  sf::RenderWindow Window(sf::VideoMode(200, 200), "SFML works!");
+  TetrisGame Game;
+
+  bool Quit = false;
+  sf::Clock Clock;
+  while (!Quit && Window.isOpen()) {
+    sf::Event Event;
+    while (Window.pollEvent(Event)) {
+      Quit = !Game.handleEvent(Event);
     }
 
-    window.clear();
-    window.draw(shape);
-    window.display();
+    Game.update(Clock.restart());
+    Window.clear();
+    Game.display(Window);
+    Window.display();
   }
+
+  Window.close();
 
   return 0;
 }
