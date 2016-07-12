5415b0c0d14b4c9f3a361fb6d6769a08ebfc29e6
Ismail Badawi <ismail@badawi.io>
2016-07-12 01:54:55 -0400

    Add shapes, pick random 'next' shape and draw it.

--- tetris.cpp
+++ tetris.cpp
@@ -1,14 +1,93 @@
 #include <cstdint>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
+#include <random>
 #include <sstream>
 #include <vector>
 
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
+typedef std::vector<std::vector<int> > Tetromino;
+std::vector<Tetromino> TETROMINOES = {
+{
+  // L
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0},
+  {0, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+{
+  // T
+  {0, 0, 0, 0, 0},
+  {0, 0, 1, 0, 0},
+  {0, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+{
+  // Reverse L
+  {0, 0, 0, 0, 0},
+  {0, 1, 0, 0, 0},
+  {0, 1, 1, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+{
+  // Square
+  {0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 0},
+  {0, 0, 1, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+{
+  // Line
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 1, 0},
+  {0, 0, 0, 1, 0},
+  {0, 0, 0, 1, 0},
+  {0, 0, 0, 1, 0},
+},
+
+{
+  // S
+  {0, 0, 0, 0, 0},
+  {0, 0, 1, 1, 0},
+  {0, 1, 1, 0, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+{
+  // Reverse S
+  {0, 0, 0, 0, 0},
+  {0, 1, 1, 0, 0},
+  {0, 0, 1, 1, 0},
+  {0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0},
+},
+
+};
+
+static Tetromino& randomTetromino() {
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> dis(0, TETROMINOES.size() - 1);
+  return TETROMINOES[dis(gen)];
+}
+
 class TetrisGame {
 private:
   std::vector<std::vector<sf::Color> > Grid;
+  Tetromino *Current;
+  Tetromino *Next;
   uint64_t Score;
   uint64_t Level;
   uint64_t Lines;
@@ -17,6 +96,7 @@ public:
   static const int Rows = 20;
   static const int Cols = 10;
   TetrisGame() : Score(0), Level(1), Lines(0),
+                 Current(nullptr), Next(&randomTetromino()),
                  Grid(Rows, std::vector<sf::Color>(Cols, sf::Color::Black)) {}
   bool handleEvent(const sf::Event &Event);
   void update(sf::Time Delta);
@@ -67,6 +147,30 @@ void TetrisGame::display(sf::RenderWindow &Window, sf::Font &Font) {
     }
   }
 
+  sf::RectangleShape NextBox(sf::Vector2f(BlockSize * 5, BlockSize * 5));
+  NextBox.setOutlineColor(sf::Color::White);
+  NextBox.setOutlineThickness(3);
+  NextBox.setFillColor(sf::Color::Black);
+  unsigned int NextBoxX = Width / 2 + (Width / 2 - BlockSize * 5) / 4;
+  unsigned int NextBoxY = Height / 8;
+  NextBox.setPosition(NextBoxX, NextBoxY);
+  Window.draw(NextBox);
+
+  for (int i = 0; i < Next->size(); ++i) {
+    for (int j = 0; j < (*Next)[0].size(); ++j) {
+      if ((*Next)[i][j]) {
+        sf::RectangleShape Block(sf::Vector2f(BlockSize, BlockSize));
+        Block.setOutlineColor(sf::Color::Black);
+        Block.setOutlineThickness(2);
+        Block.setFillColor(sf::Color::White);
+        Block.setPosition(
+            NextBoxX + j * BlockSize,
+            NextBoxY + i * BlockSize);
+        Window.draw(Block);
+      }
+    }
+  }
+
   unsigned int FontSize = 50;
   sf::Text ScoreLabel("Score", Font, FontSize);
   sf::Text ScoreValue(formatInt(Score), Font, FontSize);
@@ -92,6 +196,7 @@ void TetrisGame::display(sf::RenderWindow &Window, sf::Font &Font) {
 }
 
 int main() {
+  std::srand(std::time(0));
   sf::RenderWindow Window(sf::VideoMode(1024, 768), "Tetris");
   sf::Font Font;
   if (!Font.loadFromFile("Arial.ttf")) {
